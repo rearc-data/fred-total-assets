0ad13c06baf2a8bf1ce7242307a04828a9b60aef
Ayush Varma <avarma6@illinois.edu>
2020-07-20 15:21:15 -0500

    Fix cron and able to view files

--- pre-processing/pre-processing-code/source_data.py
+++ pre-processing/pre-processing-code/source_data.py
@@ -9,7 +9,7 @@ def data_to_s3(frmt):
     # throws error occured if there was a problem accessing data
     # otherwise downloads and uploads to s3
 
-    source_dataset_url = 'https://fred.stlouisfed.org/graph/fredgraph.csv'
+    source_dataset_url = 'https://fred.stlouisfed.org/graph/fredgraph'
     url_end = '?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1168&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=WALCL&scale=left&cosd=2002-12-18&coed=2020-07-15&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Weekly%2C%20As%20of%20Wednesday&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=2020-07-17&revision_date=2020-07-17&nd=2002-12-18'
 
     try:
@@ -22,27 +22,26 @@ def data_to_s3(frmt):
         raise Exception('URLError: ', e.reason, frmt)
 
     else:
-      #  data_set_name = os.environ['DATA_SET_NAME']
+        data_set_name = os.environ['DATA_SET_NAME']
         data_set_name = 'AVtest'
         filename = data_set_name + frmt
-        file_location = 'C:/Users/Ayush Varma/Desktop/' + filename
+        file_location = '/tmp/' + filename
 
         with open(file_location, 'wb') as f:
-            print('hi')
             f.write(response.read())
             f.close()
 
         # variables/resources used to upload to s3
-        # s3_bucket = os.environ['S3_BUCKET']
-        # new_s3_key = data_set_name + '/dataset/'
-        # s3 = boto3.client('s3')
+        s3_bucket = os.environ['S3_BUCKET']
+        new_s3_key = data_set_name + '/dataset/'
+        s3 = boto3.client('s3')
 
-        # s3.upload_file(file_location, s3_bucket, new_s3_key + filename)
+        s3.upload_file(file_location, s3_bucket, new_s3_key + filename)
 
         print('Uploaded: ' + filename)
 
         # deletes to preserve limited space in aws lamdba
-        # os.remove(file_location)
+        os.remove(file_location)
 
         # dicts to be used to add assets to the dataset revision
         # return {'Bucket': s3_bucket, 'Key': new_s3_key + filename}
@@ -62,6 +61,3 @@ def source_dataset():
 
     # asset_list is returned to be used in lamdba_handler function
     return asset_list
-
-
-source_dataset()
